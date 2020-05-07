0bb26c6ae74ef37adf2f285b5df6c6b066967afa
Mathew Begg <mbegg@ryerson.ca>
2020-05-07 01:35:01 -0400

    voting

--- superfight-client/src/app/boards/debate-board/debate-board.component.ts
+++ superfight-client/src/app/boards/debate-board/debate-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Card } from 'src/app/game.models';
 
 @Component({
@@ -10,8 +10,24 @@ export class DebateBoardComponent implements OnInit {
   @Input() fighterA: Card[];
   @Input() fighterB: Card[];
   @Input() votable = false;
+  @Output() vote = new EventEmitter<string>();
+  currentVote = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  setVote(fighter: string) {
+    if (this.votable) {
+      this.currentVote = fighter;
+    }
+  }
+
+  lockVote() {
+    if (this.currentVote === 'A' || this.currentVote === 'B') {
+      this.vote.emit(this.currentVote);
+    } else {
+      console.error('you must select a fighter to vote for');
+    }
+  }
 }
