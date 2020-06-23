42eedc8799287efcb24f0cfcdc6e74106170eec1
Mathew <mathew.r.begg@gmail.com>
2020-06-22 23:59:37 -0600

    prettier

--- scripts/migrate-aws-to-mongo-cloud.js
+++ scripts/migrate-aws-to-mongo-cloud.js
@@ -6,19 +6,17 @@ AWS.config.update({
 });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-mongoUri = 'mongodb+srv://mathewbegg:<password>-uiike.mongodb.net/SuperfightDB?retryWrites=true&w=majority';
+mongoUri =
+  'mongodb+srv://mathewbegg:<password>-uiike.mongodb.net/SuperfightDB?retryWrites=true&w=majority';
 
-MongoClient.connect(
-  mongoUri,
-  { useUnifiedTopology: true },
-  (err, client) => {
-    if (err) console.error(err);
-    console.log('Connected to MongoDB');
-    const collection = client.db('superfightDB').collection('whiteCatalogue');
-    const yeet = collection.find({text: 'Abraham Lincoln'}).toArray.forEach(x => {
+MongoClient.connect(mongoUri, { useUnifiedTopology: true }, (err, client) => {
+  if (err) console.error(err);
+  console.log('Connected to MongoDB');
+  const collection = client.db('superfightDB').collection('whiteCatalogue');
+  const yeet = collection
+    .find({ text: 'Abraham Lincoln' })
+    .toArray.forEach((x) => {
       console.log('yeet', x);
     });
-    // console.log('yeet', yeet);
-    client.close();
-  }
-);
+  client.close();
+});
