6b14f0aa63e13f1b97a01188fb0e848867e3b244
Mathew Begg <mbegg@ryerson.ca>
2020-07-05 22:26:45 -0400

    updated address to new EC2 instance

--- superfight-client/src/environments/environment.prod.ts
+++ superfight-client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverAddress: 'ec2-35-182-177-193.ca-central-1.compute.amazonaws.com:3000',
+  serverAddress: 'ec2-15-222-253-94.ca-central-1.compute.amazonaws.com:3000',
   mockNames: null,
 };
