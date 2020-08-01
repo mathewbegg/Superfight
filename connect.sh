6b2a32bfcaa47877037f89ad1f01e335ae1ece78
Mathew Begg <mbegg@ryerson.ca>
2020-08-01 16:07:51 -0400

    connecting to superfight-server.mathewbegg.com

--- connect.sh
+++ connect.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i "C:/Users/M_Beg/.ssh/superfight-server.pem" ec2-user@ec2-15-222-253-94.ca-central-1.compute.amazonaws.com
+ssh -i "C:/Users/M_Beg/.ssh/superfight-server.pem" ec2-user@superfight-server.mathewbegg.com
--- superfight-client/src/environments/environment.prod.ts
+++ superfight-client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverAddress: 'ec2-15-222-253-94.ca-central-1.compute.amazonaws.com:3000',
+  serverAddress: 'http://superfight-server.mathewbegg.com:3000',
   mockNames: null,
 };
